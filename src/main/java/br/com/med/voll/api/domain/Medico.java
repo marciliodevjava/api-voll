1beef52d68ab80a9785942bf2b52813594ca3867
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2023-02-14 22:18:28 -0300

    Atualização

--- src/main/java/br/com/med/voll/api/domain/Medico.java
+++ src/main/java/br/com/med/voll/api/domain/Medico.java
@@ -29,6 +29,7 @@ public class Medico {
         this.nome = dados.nome();
         this.email = dados.email();
         this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
     }
 }
